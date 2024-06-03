0d3b33b16f9f03ea29fb6c2f4476fb67f0502fb7
Halozhan <juwonhwang0411@gmail.com>
2024-06-03 22:55:09 +0900

    Shelf, YoloObject 파일로 분리하고 좀 더 객체지향 코드로 작성한다.

---
+++ Shelf.py
@@ -0,0 +1,75 @@
+import cv2
+import cv2.typing
+from YoloObject import YoloObject
+
+
+class Shelf:
+    """
+    선반 클래스
+    x1: 좌측 상단 x 좌표
+    y1: 좌측 상단 y 좌표
+    x2: 우측 하단 x 좌표
+    y2: 우측 하단 y 좌표
+    """
+    def __init__(self, x1: int, y1: int, x2: int, y2: int):
+        self.x1 = min(x1, x2)  # 좌측 상단 x 좌표
+        self.y1 = min(y1, y2)  # 좌측 상단 y 좌표
+        self.x2 = max(x1, x2)  # 우측 하단 x 좌표
+        self.y2 = max(y1, y2)  # 우측 하단 y 좌표
+        self.width = self.x2 - self.x1  # 선반의 너비
+        self.height = self.y2 - self.y1  # 선반의 높이
+
+        self.index = 0  # 물체를 배치할 곳의 인덱스
+        self.MARGIN = 5  # 물체와의 간격
+
+    def __str__(self):
+        return f'x1: {self.x1}, y1: {self.y1}, x2: {self.x2}, y2: {self.y2}'
+
+    def __check_placement(self, obj: YoloObject) -> bool:
+        # 물체의 너비와 높이가 남는 공간보다 작거나 같으면
+        if obj.width <= self.width - self.index and obj.height <= self.height:
+            return True
+        else:
+            return False
+
+    def __check_position(self, obj: YoloObject) -> list:
+        # 물체를 배치할 위치를 계산한다.
+        좌측상단x = self.x1 + self.index  # 좌측상단x = 선반의 좌측상단x + 인덱스
+        좌측상단y = self.y2 - obj.height  # 좌측상단y = 선반의 우측하단y - 물체의 높이
+        우측하단x = self.x1 + self.index + obj.width  # 우측하단x = 선반의 좌측상단x + 인덱스 + 물체의 너비
+        우측하단y = self.y2  # 우측하단y = 선반의 우측하단y
+        return [좌측상단x, 좌측상단y, 우측하단x, 우측하단y]
+
+    def __add_index(self, obj: YoloObject):
+        # 물체를 배치한 후 인덱스를 추가한다.
+        self.index += obj.width
+        self.index += self.MARGIN
+
+    def print_capacity(self):
+        # 선반의 용량을 출력한다.
+        print(f"선반의 용량: {self.index}/{self.width}")
+
+    def add_object_in_image(self,
+                            image: cv2.typing.MatLike,
+                            obj: YoloObject) -> bool:
+        # 물체를 선반에 추가한다.
+        if self.__check_placement(obj):
+            좌측상단x, 좌측상단y, 우측하단x, 우측하단y = self.__check_position(obj)
+            """
+            필요 없어 보이는 코드 같아요
+            # 물체가 선반 영역을 초과하지 않도록 확인
+            # if 좌측상단y < 0:
+            #     좌측상단y = 0
+            #     우측하단y = obj.height
+
+            # if 좌측상단x < 0 or 우측하단x > image.shape[1] or 좌측상단y < 0 or 우측하단y > image.shape[0]:
+            #     print(f"객체 {index + 1}: 선반 영역을 초과하여 배치할 수 없습니다.")
+            #     return False
+            """
+
+            # 객체의 이미지를 전체 그림 중 선반 위치에 추가한다.
+            image[좌측상단y:우측하단y, 좌측상단x:우측하단x] = obj.image
+            self.__add_index(obj)
+            return True
+        else:
+            return False
--- app.py
+++ app.py
@@ -1,46 +1,34 @@
+from typing import List
 from flask import Flask, request, jsonify, send_from_directory
 from ultralytics import YOLO
 import os
 import cv2
 from flask_cors import CORS, cross_origin
 import uuid
+from Shelf import Shelf  # Shelf 클래스를 가져온다.
+from YoloObject import YoloObject  # YoloObject 클래스를 가져온다.
 
-app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+app = Flask(__name__)  # Flask 앱 생성
+CORS(app, resources={r"/*": {"origins": "*"}})  # CORS 설정
 
 # YOLO 모델 로드
 model = YOLO('yolov8x-seg.pt')
 
-# 선반 클래스 정의
-class Shelf:
-    def __init__(self, x1, y1, x2, y2):
-        self.x1 = min(x1, x2)
-        self.y1 = min(y1, y2)
-        self.x2 = max(x1, x2)
-        self.y2 = max(y1, y2)
-        self.width = self.x2 - self.x1
-        self.height = self.y2 - self.y1
 
-    def __str__(self):
-        return f'x1: {self.x1}, y1: {self.y1}, x2: {self.x2}, y2: {self.y2}'
+def create_shelf_list() -> List[Shelf]:
+    """Shelf 클래스의 빈 리스트를 만드는 함수"""
+    return list()
 
-# 탐지된 객체를 관리하는 클래스
-class YoloObject:
-    def __init__(self, obj, image_path, width, height):
-        self.object = obj
-        self.image_path = image_path
-        self.width = width
-        self.height = height
 
-    def show(self):
-        image = cv2.imread(self.image_path)
-        cv2.imshow('Object', image)
-        cv2.waitKey(1)
-        cv2.destroyAllWindows()
+def create_yolo_list() -> List[YoloObject]:
+    """YoloObject 클래스의 빈 리스트를 만드는 함수"""
+    return list()
 
-yolo_objects = []
-shelf_objects = []
+
+shelf_objects = create_shelf_list()  # 선반 객체를 저장할 리스트
+yolo_objects = create_yolo_list()  # 탐지된 객체를 저장할 리스트
 uploaded_image_path = None  # 업로드된 이미지 경로를 저장할 변수
+
 
 @app.route('/upload', methods=['POST'])
 @cross_origin()
@@ -48,26 +36,26 @@ def upload_file():
     global uploaded_image_path  # 전역 변수 사용
     if 'file' not in request.files:
         return jsonify({"error": "파일이 없습니다."}), 400
-    
+
     file = request.files['file']
-    
+
     if file.filename == '':
         return jsonify({"error": "선택된 파일이 없습니다."}), 400
-    
+
     if file:
         filename = file.filename
         filepath = os.path.join('uploads', filename)
         os.makedirs(os.path.dirname(filepath), exist_ok=True)
         file.save(filepath)
-        
+
         uploaded_image_path = filepath  # 업로드된 이미지 경로 저장
         results = model(filepath)
-        
+
         # 탐지된 객체 처리
         image = cv2.imread(filepath)
         yolo_objects.clear()  # 이전 탐지된 객체 초기화
         detected_images = []
-        
+
         for index, r in enumerate(results[0].boxes.xyxy):  # YOLOv8에서 탐지된 객체들을 반복 처리
             x1, y1, x2, y2 = map(int, r[:4])
             cropped_img = image[y1:y2, x1:x2]
@@ -87,11 +75,13 @@ def upload_file():
                 "height": new_height
             })
 
-            yolo_objects.append(YoloObject(r, img_path, new_width, new_height))
+            yolo_objects.append(
+                YoloObject(r, cropped_img, img_path, new_width, new_height))
 
             print(f'객체 {index + 1}: {img_path}, 가로 길이: {new_width}, 세로 길이: {new_height}')
-        
+
         return jsonify({"detected_images": detected_images})
+
 
 @app.route('/upload-shelf', methods=['POST', 'OPTIONS'])
 @cross_origin()
@@ -111,6 +101,7 @@ def upload_shelf():
         return jsonify({"message": "선반 좌표가 성공적으로 업로드되었습니다.", "coordinates": coordinates}), 200
     return jsonify({"error": "좌표가 없습니다."}), 400
 
+
 @app.route('/arrange-items', methods=['POST'])
 @cross_origin()
 def arrange_items():
@@ -121,38 +112,22 @@ def arrange_items():
     if not uploaded_image_path:
         return jsonify({"error": "업로드된 이미지 경로가 없습니다."}), 400
 
-    shelf = shelf_objects[0]
+    shelf = shelf_objects[0]  # 첫 번째 선반 객체 사용
     image = cv2.imread(uploaded_image_path)  # 업로드된 원본 이미지 경로 사용
 
-    # 선반 영역을 표시
-    image[shelf.y1:shelf.y2, shelf.x1:shelf.x2] = [0, 128, 0]
+    # 선반 영역을 표시. RGB에서 G(초록) 색상으로 127로 설정
+    image[shelf.y1:shelf.y2, shelf.x1:shelf.x2] = [0, 127, 0]
 
     print(f"선반의 가로 길이: {shelf.width}")  # 선반의 너비를 출력한다.
     print(f"선반의 세로 길이: {shelf.height}")  # 선반의 높이를 출력한다.
     print(f"선반의 좌표: {shelf.x1}, {shelf.y1}, {shelf.x2}, {shelf.y2}")
 
-    index = 0  # 인덱스를 0으로 설정한다.
-
+    # 객체마다 선반에 추가할 수 있는지 확인하고 추가한다.
     for obj in yolo_objects:
-        if obj.width <= shelf.width - index:  # 물체의 너비가 남는 공간보다 작거나 같으면
-            좌측상단x = shelf.x1 + index  # 좌측상단x = 선반의 좌측상단x + 인덱스
-            좌측상단y = shelf.y2 - obj.height  # 좌측상단y = 선반의 우측하단y - 물체의 높이
-            우측하단x = shelf.x1 + index + obj.width  # 우측하단x = 선반의 좌측상단x + 인덱스 + 물체의 너비
-            우측하단y = shelf.y2  # 우측하단y = 선반의 우측하단y
-
-            # 물체가 선반 영역을 초과하지 않도록 확인
-            if 좌측상단y < 0:
-                좌측상단y = 0
-                우측하단y = obj.height
-
-            if 좌측상단x < 0 or 우측하단x > image.shape[1] or 좌측상단y < 0 or 우측하단y > image.shape[0]:
-                print(f"객체 {index + 1}: 선반 영역을 초과하여 배치할 수 없습니다.")
-                continue
-
-            object_image = cv2.imread(obj.image_path)
-            image[좌측상단y:우측하단y, 좌측상단x:우측하단x] = object_image  # 이미지에 객체를 추가한다.
-            index += obj.width  # 인덱스에 물체의 너비를 더한다.
-            print(f"선반의 용량: {index}/{shelf.width}")  # 선반의 용량을 출력한다.
+        if shelf.add_object_in_image(image, obj):  # 선반에 물체를 추가한다.
+            shelf.print_capacity()  # 선반의 용량을 출력한다.
+        else:
+            print(f"객체 {yolo_objects.index(obj) + 1}: 선반 영역을 초과하여 배치할 수 없습니다.")
 
     result_image_path = os.path.join('result', 'arranged_shelf.jpg')
     os.makedirs(os.path.dirname(result_image_path), exist_ok=True)
@@ -161,10 +136,12 @@ def arrange_items():
 
     return jsonify({"message": "정리된 이미지가 성공적으로 저장되었습니다.", "result_image": 'arranged_shelf.jpg'}), 200
 
+
 @app.route('/result/<path:filename>', methods=['GET'])
 @cross_origin()
 def serve_result_file(filename):
     return send_from_directory('result', filename)
+
 
 @app.route('/get-shelf-images', methods=['GET'])
 @cross_origin()
@@ -172,10 +149,12 @@ def get_shelf_images():
     images = [os.path.basename(obj.image_path) for obj in yolo_objects]
     return jsonify(images)
 
+
 @app.route('/upload-shelf/<path:filename>', methods=['GET'])
 @cross_origin()
 def serve_file(filename):
     return send_from_directory('upload-shelf', filename)
+
 
 @app.route('/update-objects', methods=['POST'])
 @cross_origin()
@@ -192,7 +171,8 @@ def update_objects():
 
     return jsonify({"message": "YOLO 객체 업데이트 완료"}), 200
 
-if __name__ == '__main__':
+
+def main():
     if not os.path.exists('uploads'):
         os.makedirs('uploads')
     if not os.path.exists('upload-shelf'):
@@ -200,3 +180,7 @@ if __name__ == '__main__':
     if not os.path.exists('result'):
         os.makedirs('result')
     app.run(debug=True)
+
+
+if __name__ == '__main__':
+    main()
